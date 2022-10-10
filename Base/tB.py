eaeec15e2c287fa3412175365621effc32d7233b
aK# <83608645+adnksharp@users.noreply.github.com>
2022-10-09 22:14:08 -0600

    Fix labels

--- Base/tB.py
+++ Base/tB.py
@@ -3,7 +3,7 @@ from mpl_toolkits.mplot3d import axes3d
 
 def Figure(P, P1a, P2a, P3a, P1b, P2b, P3b, P4b, P1c, P2c, P3c, P4c, a):
     d3 = plt.figure()
-    ax = plt.axes(projection='3d')
+    ax1 = plt.axes(projection='3d')
     ax1 = d3.add_subplot(111, projection='3d')
 
     ax1.plot([P1a[0],P2a[0]],[P1a[1],P2a[1]],[P1a[2],P2a[2]],color="purple", linewidth=1, linestyle="-")
@@ -33,7 +33,7 @@ def Figure(P, P1a, P2a, P3a, P1b, P2b, P3b, P4b, P1c, P2c, P3c, P4c, a):
     ax1.scatter([P2c[0],P3c[0]],[P2c[1],P3c[1]],[P2c[2],P3c[2]])
     ax1.scatter([P3c[0],P4c[0]],[P3c[1],P4c[1]],[P3c[2],P4c[2]])
 
-    ax.set_title('Robot 3/2')
+    ax1.set_title('Robot 3/2')
     ax1.set_xlabel("x")
     ax1.set_ylabel("y")
     ax1.set_zlabel("z")
